c9f5e635a093fd11ed86defc95e01e12b2f82370
Johan Lindh <johan@linkdata.se>
2025-11-05 14:17:31 +0100

    send auth location even if state was already set

--- oauth2.go
+++ oauth2.go
@@ -89,17 +89,15 @@ func (srv *Server) HandleLogin(hw http.ResponseWriter, hr *http.Request) {
 	oauth2cfg, _, location := srv.begin(hr)
 	if oauth2cfg != nil {
 		if sess := srv.Jaws.GetSession(hr); sess != nil {
-			if sess.Get(oauth2StateKey) == nil {
-				b := make([]byte, 32)
-				var n int
-				var err error
-				if n, err = rand.Read(b); srv.Jaws.Log(err) == nil {
-					state := hex.EncodeToString(b[:n])
-					sess.Set(oauth2StateKey, state)
-					sess.Set(oauth2ReferrerKey, location)
-					location = oauth2cfg.AuthCodeURL(state, srv.Options...)
-				}
+			state, _ := sess.Get(oauth2StateKey).(string)
+			if state == "" {
+				b := [32]byte{}
+				_, _ = rand.Read(b[:]) // never returns an error, always fills all of b
+				state = hex.EncodeToString(b[:])
+				sess.Set(oauth2StateKey, state)
 			}
+			sess.Set(oauth2ReferrerKey, location)
+			location = oauth2cfg.AuthCodeURL(state, srv.Options...)
 		}
 	}
 	hw.Header().Add("Location", location)
